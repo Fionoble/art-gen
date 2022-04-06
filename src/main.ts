c8cb97618a82d8d5771fa133a38d5dae7ddfc4b7
Phiroze Noble <p.noble.dennis@gmail.com>
2022-04-05 22:19:49 -0400

    Initial commit

---
+++ src/main.ts
@@ -0,0 +1,12 @@
+import './style.css'
+
+const dlBtn = document.getElementById('downloadButton')
+const canvas = document.getElementById('myCanvas') as HTMLCanvasElement
+const context = canvas?.getContext('2d')
+
+dlBtn?.addEventListener('onClick', ()=> {
+  // TODO download canvas result here
+})
+
+// do art creation here with context
+
