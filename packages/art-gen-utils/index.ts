c4ae7c919defe8255cfcc3e616e1b3a0b49b0fc2
Phiroze Noble <p.noble.dennis@gmail.com>
2022-04-07 11:52:59 -0400

    Create packages; add more utils

---
+++ packages/art-gen-utils/index.ts
@@ -0,0 +1,52 @@
+export function randomHex() {
+  return `#${Math.floor(Math.random()*16777215).toString(16)}`
+}
+
+export function randomRGB(asObj = false): String | any {
+  const red = Math.round(Math.random()*255)
+  const green = Math.round(Math.random()*255)
+  const blue = Math.round(Math.random()*255)
+
+  return asObj ? {red, green, blue} : `rgb(${red}, ${green}, ${blue})`
+}
+
+export function randomRGBWithinRange(r: number, g: number, b: number, range: number) {
+  const getWithRange = (v: number) => v - (range/2) + (Math.random()*range);
+  return `rgb(${getWithRange(r)}, ${getWithRange(g)}, ${getWithRange(b)})`
+}
+
+export type RectArgs = { 
+  x: number;
+  y: number;
+  width: number;
+  height: number;
+  rotation?: number;
+  color?: string;
+}
+
+
+export function makeRectWithRotation(context: CanvasRenderingContext2D, args: RectArgs) {
+  const {x,y,width,height,rotation, color} = args;
+
+  // first save the untranslated/unrotated context
+  context.save();
+
+  context.beginPath();
+  
+  // move the rotation point to the center of the rect
+  context.translate( x+width/2, y+height/2 );
+
+  // rotate the rect
+  if(rotation)context.rotate(rotation*Math.PI/180);
+
+  // draw the rect on the transformed context
+  // Note: after transforming [0,0] is visually [x,y]
+  //       so the rect needs to be offset accordingly when drawn
+  context.rect( -width/2, -height/2, width,height);
+
+  context.fillStyle = color ? color : randomRGB();
+  context.fill();
+
+  // restore the context to its untranslated/unrotated state
+  context.restore();
+}
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,43 @@
 import './style.css'
+import {randomRGB, randomRGBWithinRange, makeRectWithRotation} from '../packages/art-gen-utils';
 
 const dlBtn = document.getElementById('downloadButton')
 const canvas = document.getElementById('myCanvas') as HTMLCanvasElement
-const context = canvas?.getContext('2d')
+const context = canvas.getContext('2d')
 
-dlBtn?.addEventListener('onClick', ()=> {
-  // TODO download canvas result here
+dlBtn?.addEventListener('click', ()=> {
+    const l = document.createElement('a')
+    l.download = 'filename.png'
+    l.href = canvas.toDataURL()
+    l.click()
+    console.log(l.href)
 })
 
-// do art creation here with context
+// set size
+canvas.width  = window.innerWidth
+canvas.height = window.innerHeight
 
+// do art creation here with context
+const draw = (ctx: CanvasRenderingContext2D) => {
+  const winW = window.innerWidth;
+  const winH = window.innerHeight;
+
+  const {red, green, blue} = randomRGB(true);// as {red: number, blue: number, green: number};
+  const numberOfBoxes = 1000;
+  ctx.fillStyle = `rgb(${red}, ${green}, ${blue})`
+  ctx.fillRect(0, 0, winW, winH)
+
+  for(let i = 0; i < numberOfBoxes; i++) {
+    const sqSize = Math.round(Math.random() * 100)
+    makeRectWithRotation(ctx, {
+      x: Math.round(Math.random() * winW), 
+      y: Math.round(Math.random() * winH),
+      width: sqSize,
+      height: sqSize,
+      rotation: Math.round(Math.random() * 360),
+      color: randomRGBWithinRange(red, green, blue, 25)
+    })
+  }
+}
+
+if(context) draw(context)
