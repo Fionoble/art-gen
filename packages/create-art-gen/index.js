9dfaee749e31e7dd99ca8ec8c8a4351ada939200
Phiroze Noble <p.noble.dennis@gmail.com>
2022-10-09 15:27:43 -0400

    Start working on npx command

--- packages/create-art-gen/index.js
+++ packages/create-art-gen/index.js
@@ -1,4 +1,21 @@
 #!/usr/bin/env node
-
 'use strict';
 
+const prompt = require('prompt-sync')();
+const fs = require('fs')
+
+console.log('You\'re creating a new JS Generated Art! 😁')
+console.log('What would you like to call this piece? 🤔')
+
+const title = prompt('Title: ');
+
+const cleanName = title.trim().replaceAll(' ', '-')
+console.log(`Creating project in ./${cleanName}`)
+fs.mkdir(`./${cleanName}`, {}, (err) => {
+  if(err) throw(err)
+  else {
+    console.log(`🎉🎉🎉 Project successfully created! 🎉🎉🎉 
+Enjoy make some art-gen and share your work! 🧑‍🎨
+`)
+  }
+})
